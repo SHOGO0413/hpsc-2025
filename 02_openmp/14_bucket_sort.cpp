24b34635e4f7173cb2a787239f6360b7c5abb20b
SHOGO0413 <89981325+SHOGO0413@users.noreply.github.com>
2025-04-21 01:59:30 +0900

    Update 14_bucket_sort.cpp

--- 02_openmp/14_bucket_sort.cpp
+++ 02_openmp/14_bucket_sort.cpp
@@ -1,23 +1,37 @@
 #include <cstdio>
 #include <cstdlib>
 #include <vector>
+#include <omp.h>
 
 int main() {
-  int n = 50;
+  int n = 20;
   int range = 5;
+  int tmp[range];
   std::vector<int> key(n);
+
   for (int i=0; i<n; i++) {
     key[i] = rand() % range;
     printf("%d ",key[i]);
   }
   printf("\n");
 
-  std::vector<int> bucket(range,0); 
+  std::vector<int> bucket(range,0);
+
   for (int i=0; i<n; i++)
     bucket[key[i]]++;
   std::vector<int> offset(range,0);
-  for (int i=1; i<range; i++) 
-    offset[i] = offset[i-1] + bucket[i-1];
+
+  #pragma omp parallel
+  for (int j =1; j<range; j<<=1){
+    #pragma omp parallel for
+    for(int i = 0; i<range; i++){
+        tmp[i]=offset[i];
+    }
+    #pragma omp parallel for
+    for (int i=j; i<range; i++)
+        offset[i] = tmp[i-1]+bucket[i-1];
+  }
+
   for (int i=0; i<range; i++) {
     int j = offset[i];
     for (; bucket[i]>0; bucket[i]--) {
