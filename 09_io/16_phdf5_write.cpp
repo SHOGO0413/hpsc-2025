cef2c4964fd81b1428dbe883b5d1dde72e8c5ab8
SHOGO0413 <89981325+SHOGO0413@users.noreply.github.com>
2025-05-22 14:58:15 +0900

    Update 16_phdf5_write.cpp

--- 09_io/16_phdf5_write.cpp
+++ 09_io/16_phdf5_write.cpp
@@ -19,7 +19,7 @@ int main (int argc, char** argv) {
   for(int i=0; i<2; i++) offset[i] *= Nlocal[i];
   hsize_t count[2] = {2,2};
   hsize_t stride[2] = {2,2};
-  vector<int> buffer(Nlocal[0]*Nlocal[1],mpirank);
+  vector<int> buffer(count[0]*count[1]*Nlocal[0]*Nlocal[1],mpirank);
   hid_t plist = H5Pcreate(H5P_FILE_ACCESS);
   H5Pset_fapl_mpio(plist, MPI_COMM_WORLD, MPI_INFO_NULL);
   hid_t file = H5Fcreate("data.h5", H5F_ACC_TRUNC, H5P_DEFAULT, plist);
