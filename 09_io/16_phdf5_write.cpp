f2dcd8a8b149c64dcfcf28b49a9c91e2da909866
SHOGO0413 <89981325+SHOGO0413@users.noreply.github.com>
2025-05-22 17:13:09 +0900

    Update 16_phdf5_write.cpp

--- 09_io/16_phdf5_write.cpp
+++ 09_io/16_phdf5_write.cpp
@@ -20,8 +20,7 @@ int main (int argc, char** argv) {
   for(int i=0; i<2; i++) offset[i] *= block[i];
   hsize_t count[2] = {2,2};
   hsize_t stride[2] = {2,2};
-  hsize_t localsize[2] = {Nlocal[0]*1.5,Nlocal[1]*1.5};
-  vector<int> buffer(localsize[0]*localsize[1],mpirank);
+  vector<int> buffer(Nlocal[0]*nNlocal[1],mpirank);
   hid_t plist = H5Pcreate(H5P_FILE_ACCESS);
   H5Pset_fapl_mpio(plist, MPI_COMM_WORLD, MPI_INFO_NULL);
   hid_t file = H5Fcreate("data.h5", H5F_ACC_TRUNC, H5P_DEFAULT, plist);
@@ -29,7 +28,7 @@ int main (int argc, char** argv) {
   hid_t localspace = H5Screate_simple(2, localsize, NULL);
   hid_t dataset = H5Dcreate(file, "dataset", H5T_NATIVE_INT, globalspace,
 			    H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT);
-  H5Sselect_hyperslab(globalspace, H5S_SELECT_SET, offset, stride, count, block);
+  H5Sselect_hyperslab(globalspace, H5S_SELECT_SET, offset, stride, count, Nlocal);
   H5Pclose(plist);
   plist = H5Pcreate(H5P_DATASET_XFER);
   H5Pset_dxpl_mpio(plist, H5FD_MPIO_COLLECTIVE);
