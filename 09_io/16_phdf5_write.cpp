a39d516763617d800b9b5d627fead41942f17e45
SHOGO0413 <89981325+SHOGO0413@users.noreply.github.com>
2025-05-22 14:39:50 +0900

    Update 16_phdf5_write.cpp

--- 09_io/16_phdf5_write.cpp
+++ 09_io/16_phdf5_write.cpp
@@ -19,7 +19,7 @@ int main (int argc, char** argv) {
   for(int i=0; i<2; i++) offset[i] *= Nlocal[i];
   hsize_t count[2] = {2,2};
   hsize_t stride[2] = {2,2]};
-  hsize_t block = {Nlocal[0]*2,Nlocal[1]*2}
+  hsize_t block[2] = {Nlocal[0]*2,Nlocal[1]*2}
   vector<int> buffer(Nlocal[0]*Nlocal[1],block);
   hid_t plist = H5Pcreate(H5P_FILE_ACCESS);
   H5Pset_fapl_mpio(plist, MPI_COMM_WORLD, MPI_INFO_NULL);
